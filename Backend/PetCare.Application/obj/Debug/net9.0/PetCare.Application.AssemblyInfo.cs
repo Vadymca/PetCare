682a3444eb23e2848381ca6e64fe9f7940614d04
Vadim <vadimancuta@gmail.com>
2025-08-03 18:18:24 +0300

    fix: resolve merge conflicts

--- Backend/PetCare.Application/obj/Debug/net9.0/PetCare.Application.AssemblyInfo.cs
+++ Backend/PetCare.Application/obj/Debug/net9.0/PetCare.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCare.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0182c2373b9abf41e6945e03219f24f039b13075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff13fc979276ef58e21fcacbfdb80f11f3b04b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCare.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCare.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
