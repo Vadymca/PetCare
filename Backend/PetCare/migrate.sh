e8dcbf5d5371ef89fb5102d2986cbbf9631d2d3c
Vadim <vadimancuta@gmail.com>
2025-10-10 18:14:09 +0300

    update docker

--- Backend/PetCare/migrate.sh
+++ Backend/PetCare/migrate.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-echo "Waiting for database..."
-until nc -z db 5432; do
+# $1 = host, $2 = port
+
+HOST=$1
+PORT=$2
+
+echo "Waiting for database $HOST:$PORT..."
+
+while ! nc -z $HOST $PORT; do
   sleep 2
 done
-echo "Database ready. Applying migrations..."
+
+echo "Database ready! Applying migrations..."
+
+# Виконуємо EF Core міграції в SDK stage
+dotnet tool restore
 dotnet ef database update \
   --project /src/PetCare.Infrastructure/PetCare.Infrastructure.csproj \
   --startup-project /src/PetCare.Api/PetCare.Api.csproj
+
 echo "Migrations applied."
