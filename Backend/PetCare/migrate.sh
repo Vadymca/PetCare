017c57888ef760ca5e8d7be5a09586e6fc3e7579
Vadim <vadimancuta@gmail.com>
2025-10-10 17:31:37 +0300

    update

--- Backend/PetCare/migrate.sh
+++ Backend/PetCare/migrate.sh
@@ -1,21 +1,10 @@
-#!/bin/bash
-set -e
-
-HOST=db
-PORT=5432
-
-echo "Waiting for database $HOST:$PORT..."
-
-# Чекаємо поки БД буде доступна
-while ! nc -z $HOST $PORT; do
-  echo "Waiting for database..."
+#!/bin/sh
+echo "Waiting for database..."
+until nc -z db 5432; do
   sleep 2
 done
-
 echo "Database ready. Applying migrations..."
-
 dotnet ef database update \
   --project /src/PetCare.Infrastructure/PetCare.Infrastructure.csproj \
   --startup-project /src/PetCare.Api/PetCare.Api.csproj
-
 echo "Migrations applied."
