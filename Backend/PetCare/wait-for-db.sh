7d26fd0ee85e9dd5a670e6820ee03d0f9c2dc2db
Vadim <vadimancuta@gmail.com>
2025-10-11 18:48:09 +0300

    update

--- Backend/PetCare/wait-for-db.sh
+++ Backend/PetCare/wait-for-db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Параметри
+# Parameters
 HOST="$1"
 PORT="$2"
 shift 2
@@ -11,12 +11,5 @@ while ! nc -z $HOST $PORT; do
   sleep 1
 done
 
-echo "База доступна. Виконуємо міграції..."
-dotnet tool restore # якщо використовується manifest
-dotnet ef database update \
-    --project PetCare.Infrastructure/PetCare.Infrastructure.csproj \
-    --startup-project PetCare.Api/PetCare.Api.csproj \
-    --configuration Release
-
-echo "Запускаємо додаток..."
+echo "База доступна. Запускаємо додаток..."
 exec $CMD
