e4999c1230f73bb354b00cc544e8319ff1a3fca1
Vadim <vadimancuta@gmail.com>
2025-10-11 21:41:26 +0300

    update docker

--- Backend/PetCare/wait-for-db.sh
+++ Backend/PetCare/wait-for-db.sh
@@ -1,16 +1,13 @@
-#!/bin/sh
-# wait-for-db.sh
-
+#!/bin/bash
 host="$1"
 port="$2"
 shift 2
 cmd="$@"
 
-echo "Waiting for database $host:$port..."
-
-while ! nc -z $host $port; do
+until nc -z "$host" "$port"; do
+  >&2 echo "Waiting for database $host:$port..."
   sleep 1
 done
 
-echo "Database is up - starting command: $cmd"
+>&2 echo "Database is up - starting command: $cmd"
 exec $cmd
