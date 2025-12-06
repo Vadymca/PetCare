04f4d994817028f9b7875570d9d3b55c5a12210f
Alla Kokhaniuk <alla.kohanjuk@gmail.com>
2025-12-07 01:18:33 +0200

    Configure environment.prod.ts for Render deployment

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://json-server:3000' // URL для Docker
+// };
+
 export const environment = {
   production: true,
-  apiUrl: 'http://json-server:3000' // URL для Docker
+  apiUrl:
+    process.env['API_BASE_URL'] ||
+    'https://api-dobrodiy.kn314-uz.keenetic.pro/',
 };
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
   apiUrl: '/api',
-  //apiUrl: 'https://api-dobrodiy.kn314-uz.keenetic.pro/api', // Локальний URL для розробки
-  //apiUrl: 'http://localhost:5000',
 };
