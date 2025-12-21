11fa24c5837150121134c88d616f19e2ecc3554b
Vadim <vadimancuta@gmail.com>
2025-12-21 14:00:49 +0200

    fix6

--- Backend/PetCare/PetCare.Infrastructure/Services/JwtService.cs
+++ Backend/PetCare/PetCare.Infrastructure/Services/JwtService.cs
@@ -166,6 +166,7 @@ public sealed class JwtService : IJwtService
             SameSite = SameSiteMode.None,
             Expires = DateTime.UtcNow.AddDays(this.refreshExpirationDays),
             Path = "/",
+            Domain = ".phfk.college",
         };
 
         response.Cookies.Append("refresh_token", token, options);
